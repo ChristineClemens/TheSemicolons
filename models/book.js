6f131869e3cc4b273be48f2d79368e4f74c866a3
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-07-03 14:16:01 -0400

    Added get functions to book model

--- models/book.js
+++ models/book.js
@@ -7,4 +7,23 @@ class BookModel {
         await db.close()
         return userBooks
     }
+
+    async getBooksByGenre(genreName){
+        let db = new orm("mylibrary")
+        let genreBooks = await db.selectSome("books", {genre: genreName})
+        await db.close()
+        return genreBooks
+    }
+    async getBooksByTitle(bookTitle){
+        let db = new orm("mylibrary")
+        let books = await db.selectSome("books", {title: bookTitle})
+        await db.close()
+        return books
+    }
+    async getBooksByAuthor(bookAuthor){
+        let db = new orm("mylibrary")
+        let books = await db.selectSome("books", {author: bookAuthor})
+        await db.close()
+        return books
+    }
 }
