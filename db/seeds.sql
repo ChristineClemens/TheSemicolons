3b988bef994522190beb5f74cb6692e90f5f96a6
Christine Clemens <christineclemens.tesol@gmail.com>
2020-07-03 13:32:38 -0400

    Inserted example user and book values in seed.sql file.

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (auth_id) VALUES (auth0|5efe8a69a15b7b001361ce52);
+
+INSERT INTO books (title, author, genre, description, page_count, book_cover, user_added, date_added)
+VALUE ('The Old Man and the Sea', 'Earnest Miller Hemingway', 'Fiction', 'This is an Egyptian edition of one 
+of the most internationally celebrated works of fiction, Hemingway’s The old Man and the Sea. 
+A Pulitzer-award and - Noble-prize winner, this work has cer- tainly become so extensively known all 
+over the world, receiving a myriad of critical studies and translations. Hemingway himself could have 
+never imagined such a sweeping success for his novel- la, devised while temporarily staying (for a few
+ weeks) at a fishing village in Cuba. It is not simply the story of a big fish catch, but it is the 
+ story of a touching human relationship between old age and younghood, not to mention its highly 
+ philosophical message that it carries. All this, apart from other equally significant factors, makes 
+ Hemingway’s work worth reading and contemplating.', '84', 'https://books.google.ca/books/content?id=rl9kCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE71nstrPt9UYrsiYblsXBstNzswfOsxOBexTXxFb17OyzUs9GRfU5atDg3p5lBi0aoJfZuWc2kfLbNP0hgOk3hVEGW2GqWWyM_icGVoXAEGLWpyEDP5E6Xd-s3nWitOwggLB3eYO', '2', CURDATE() );
