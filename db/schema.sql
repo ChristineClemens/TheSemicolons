aa17c84c4b8f9533fe0d3b2518a63b0af5741126
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-03 11:38:40 -0400

    Built initial DB

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,28 @@
-USE 
+DROP DATABASE IF exists mylibrary;
+CREATE DATABASE mylibrary;
+USE mylibrary;
+
+CREATE TABLE users(
+	id integer auto_increment primary key,
+    auth_id varchar (255) not null
+    );
+    
+CREATE TABLE books(
+	id integer auto_increment primary key,
+    title varchar (255),
+    author varchar (255),
+    genre varchar (255),
+    description varchar (255),
+    page_count integer, 
+    book_cover varchar(255),
+    user_added integer,
+    foreign key (user_added) references users(id)
+    );
+
+    
+    
+    
+    
+
+
+ 
