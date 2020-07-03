fecdc1e7329847f51502ac0101d8d73b7e68b184
Christine Clemens <christineclemens.tesol@gmail.com>
2020-07-03 13:46:28 -0400

    Added a second set of example values to seeds.sql file.

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,5 @@
 DROP DATABASE IF EXISTS mylibrary;
 
-<<<<<<< HEAD
 CREATE DATABASE mylibrary;
 
 USE mylibrary;
@@ -22,21 +21,3 @@ user_added INTEGER,
 date_added INTEGER,
 FOREIGN KEY (user_added) REFERENCES users(id)
 );
-=======
-CREATE TABLE users(
-	id integer auto_increment primary key,
-    auth_id varchar (255) not null
-    );
-    
-CREATE TABLE books(
-	id integer auto_increment primary key,
-    title varchar (255),
-    author varchar (255),
-    genre varchar (255),
-    description varchar (255),
-    page_count integer, 
-    book_cover varchar(255),
-    user_added integer,
-    foreign key (user_added) references users(id)
-    );
->>>>>>> master
