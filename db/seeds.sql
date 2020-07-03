9d46603d0ec443b6f02f36d95914e1c0310c336a
Christine Clemens <christineclemens.tesol@gmail.com>
2020-07-03 13:52:02 -0400

    Added second example to the seeds.sql file.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,11 @@
 INSERT INTO users (auth_id) VALUES (auth0|5efe8a69a15b7b001361ce52);
+INSERT INTO users (auth_id) VALUES (google-oauth2|107478579241719314235);
 
 INSERT INTO books (title, author, genre, description, page_count, book_cover, user_added, date_added)
-VALUE ('The Old Man and the Sea', 'Earnest Miller Hemingway', 'Fiction', 'This is an Egyptian edition of one 
+VALUES ('A Tale for the Time Being', 'Ruth Ozeki', 'Fiction', '“A time being is someone who lives in 
+time, and that means you, and me, and every one of us who is, or was, or ever will be.”', '398', 'https://books.google.ca/books/content?id=AkczPdrwq80C&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE71PJC0Jtk4zkEX7VeDw9Dn5b0l56cPriJkzQV6xofBpasIUQ4CpOhEbH7dzsC9a0Ua95fy7Iw3zdf_G0nzqmBNKnmPD9Ssv_SpVpVTPzuGnXy3fypxVpwvmMU4yDtHiC36x0QMU', '2', CURDATE() );
+INSERT INTO books (title, author, genre, description, page_count, book_cover, user_added, date_added)
+VALUES ('The Old Man and the Sea', 'Earnest Miller Hemingway', 'Fiction', 'This is an Egyptian edition of one 
 of the most internationally celebrated works of fiction, Hemingway’s The old Man and the Sea. 
 A Pulitzer-award and - Noble-prize winner, this work has cer- tainly become so extensively known all 
 over the world, receiving a myriad of critical studies and translations. Hemingway himself could have 
@@ -9,4 +13,4 @@ never imagined such a sweeping success for his novel- la, devised while temporar
  weeks) at a fishing village in Cuba. It is not simply the story of a big fish catch, but it is the 
  story of a touching human relationship between old age and younghood, not to mention its highly 
  philosophical message that it carries. All this, apart from other equally significant factors, makes 
- Hemingway’s work worth reading and contemplating.', '84', 'https://books.google.ca/books/content?id=rl9kCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE71nstrPt9UYrsiYblsXBstNzswfOsxOBexTXxFb17OyzUs9GRfU5atDg3p5lBi0aoJfZuWc2kfLbNP0hgOk3hVEGW2GqWWyM_icGVoXAEGLWpyEDP5E6Xd-s3nWitOwggLB3eYO', '2', CURDATE() );
+ Hemingway’s work worth reading and contemplating.', '84', 'https://books.google.ca/books/content?id=rl9kCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE71nstrPt9UYrsiYblsXBstNzswfOsxOBexTXxFb17OyzUs9GRfU5atDg3p5lBi0aoJfZuWc2kfLbNP0hgOk3hVEGW2GqWWyM_icGVoXAEGLWpyEDP5E6Xd-s3nWitOwggLB3eYO', '3', CURDATE() );
