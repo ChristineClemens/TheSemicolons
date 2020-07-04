e0a696806b734d60eb10550414975b739270df6b
Christine Clemens <christineclemens.tesol@gmail.com>
2020-07-04 17:03:23 -0400

    Started coding and testing fetch function with Google Books API.

--- routes/mylibrary.js
+++ routes/mylibrary.js
@@ -15,3 +15,34 @@ router.get("/mylibrary", secured(), function (req, res, next) {
 });
 
 module.exports = router;
+
+
+
+//JAMES, LOOK WHAT WE DID.
+var APIKey = "AIzaSyAVDdcFdW0ng8X7gOa1ODlAlSmCmrhtFQE";
+
+//Define title input as API call.
+async function addBook() {
+    var inputBookTitle = document.querySelector("#submitTitle").value.split(" ").join("+").toLowerCase();
+    var bookTitleSearch = `https://www.googleapis.com/books/v1/volumes?q=${inputBookTitle}&appid=${APIKey}`;
+
+    //Performed a fetch request to retrieve information about an individual volume.
+    await fetch (bookTitleSearch)
+    .then (response => response.json())
+    .then(data => {
+        console.log(data);
+
+    //Collected and defined information as a result of the API call.
+    var bookInfo = {
+        title: `${data.items[0].volumeInfo.title}`,
+        author: `${data.items[0].volumeInfo.authors}`,
+        genre: `${data.items[0].volumeInfo.categories}`,
+        description: `${data.items[0].volumeInfo.description}`,
+        page_count: `${data.items[0].volumeInfo.pageCount}`,
+        book_cover: `${data.items[0].volumeInfo.imageLinks.thumbnail}`
+    } 
+
+    //Inserted new book information into the books table.
+    var sql = `INSERT INTO books (title, author, genre, description, page_count, book_cover) 
+    VALUES (${bookInfo.title}, ${bookInfo.author}, ${bookInfo.genre}, ${bookInfo.description}, ${bookInfo.page_count}, ${bookInfo.book_cover})`
+})};
