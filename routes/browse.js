ec0cc29e19cdb86dc00199d2ee8fc53dc688464f
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-07-05 13:09:13 -0400

    Added backend for loading and rendering all books on the browse page, linked to request page

--- routes/browse.js
+++ routes/browse.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const secured = require("../lib/middleware/secured");
-
+const book = require("../models/book");
+const BookModel = new book();
 
 /* GET home page. */
-router.get("/browse", secured(), function (req, res, next) {
+router.get("/browse", secured(), async function (req, res, next) {
     console.log("browse page loaded");
-    res.render("browse", { title : "Browse Page" });
+
+    //getting all the books in the db
+    const allBooks = await BookModel.getAllBooks();
+    console.log("allBooks", allBooks);
+
+    res.render("browse", {
+        title: "Browse Page",
+        books: allBooks,
+    });
 });
 
 module.exports = router;
