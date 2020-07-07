057d53e09b7342220a4ca0a8d91d1fe5e61df250
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-07-06 21:12:04 -0400

    Added header to top of default template and removed from others

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Welcome to PageExchange" });
+    res.render("index", { title: "Welcome to PageExchange", notLoggedIn: true});
 });
 
 module.exports = router;
