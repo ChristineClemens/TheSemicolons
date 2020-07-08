29d9e68dcab0d57c80834d8a73059fdc69849e4c
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-07-07 23:13:10 -0400

    Created page to see details for book

--- routes/browse.js
+++ routes/browse.js
@@ -10,7 +10,7 @@ router.get("/browse", secured(), async function (req, res, next) {
 
     //getting all the books in the db
     let allBooks = await BookModel.getAllBooks();
-    allBooks = parseBooks(allBooks)
+    allBooks = parseBooks(allBooks);
     res.render("browse", {
         title: "Browse",
         books: allBooks,
@@ -22,7 +22,7 @@ router.get("/browse/:condition/:query", async function (req, res) {
     let query = req.params.query;
     console.log(`getting books based on search for browse page `, condition, query);
     let allBooks = await BookModel.getBooksFuzzy(condition, query);
-    allBooks = parseBooks(allBooks)
+    allBooks = parseBooks(allBooks);
     res.render("browse", {
         title: "Browse",
         books: allBooks,
@@ -30,7 +30,19 @@ router.get("/browse/:condition/:query", async function (req, res) {
     });
 });
 
-function parseBooks(bookList){
+router.get("/view_book/:id", async function (req, res) {
+    console.log("Book information page loaded", req.params.id)
+    let bookID = req.params.id;
+    let bookInformation = await BookModel.getBookFromDBID(bookID);
+    console.log('book info', bookInformation)
+    if (bookInformation) {
+        res.render("bookInformation", { book: bookInformation });
+    } else {
+        res.render("404");
+    }
+});
+
+function parseBooks(bookList) {
     return bookList.map((book) => ({
         title: book.title,
         genre: book.genre,
@@ -39,16 +51,15 @@ function parseBooks(bookList){
         possession_id: book.possession_id,
         date_added: book.date_added,
         id: book.id,
-        description: conditionalTruncate(book.description)
+        description: conditionalTruncate(book.description),
     }));
 }
 
-function conditionalTruncate(string){
-    if (string.length > 300){
-        return string.trim().substring(0, 300) + "..."
+function conditionalTruncate(string) {
+    if (string.length > 300) {
+        return string.trim().substring(0, 300) + "...";
     }
-    return string
+    return string;
 }
-
 
 module.exports = router;
